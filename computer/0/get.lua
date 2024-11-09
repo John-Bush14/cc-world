bc5e14e26b788002e810ed51f084f7d28193ff9c
John-Bush14 <joppe@stuyck.be>
2024-11-09 21:02:57 +0100

    fix: make get.lua open port before listening on it (lol)

--- computer/0/get.lua
+++ computer/0/get.lua
@@ -2,9 +2,11 @@ local query, amount = ...
 
 local item, modem, _, itemRemotePort = require("getItem")(query)
 
+modem.open(itemRemotePort)
+
 if item == nil then return end
 
-modem.transmit(itemRemotePort, 128, {count=amount, item=item, request="send"})
+modem.transmit(itemRemotePort, 128, {count=amount or 1, item=item, request="send"})
 
 print("Transmiting...")
 
