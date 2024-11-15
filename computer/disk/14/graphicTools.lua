625a4229f357bcfdfbef9540a6063bed7881c34b
John-Bush14 <joppe@stuyck.be>
2024-11-15 14:18:07 +0100

    chore(graphictools): correct tabbing :'(

--- computer/disk/14/graphicTools.lua
+++ computer/disk/14/graphicTools.lua
@@ -3,33 +3,33 @@ local M = {}
 local bigfont = require("../" .. fs.getDir(fs.find("*/bigfont.lua")[1]) .. "/bigfont")
 
 function M.extend_screen(screen)
-    function screen.setCursorPosX(x)
-        local _, y = screen.getCursorPos()
-        screen.setCursorPos(x, y)
-    end
+   function screen.setCursorPosX(x)
+      local _, y = screen.getCursorPos()
+      screen.setCursorPos(x, y)
+   end
 
-    function screen.setCursorPosY(y)
-        local x, _ = screen.getCursorPos()
-        screen.setCursorPos(x, y)
-    end
+   function screen.setCursorPosY(y)
+      local x, _ = screen.getCursorPos()
+      screen.setCursorPos(x, y)
+   end
 
-    function screen.moveCursor(x, y)
-        if type(x) == "table" then
-        y = x.y
-        x = x.x
-        end
-        local ox, oy = screen.getCursorPos()
-        screen.setCursorPos(ox+x, oy+y)
-    end
+   function screen.moveCursor(x, y)
+      if type(x) == "table" then
+      y = x.y
+      x = x.x
+      end
+      local ox, oy = screen.getCursorPos()
+      screen.setCursorPos(ox+x, oy+y)
+   end
 
-    function screen.printBig(text, size)
-        local x, y = screen.getCursorPos()
-        bigfont.writeOn(screen, size or 1, text, x, y)
-    end
+   function screen.printBig(text, size)
+      local x, y = screen.getCursorPos()
+      bigfont.writeOn(screen, size or 1, text, x, y)
+   end
 
-    function screen.drawPixel(color)
-        screen.blit("o", color, color)
-    end
+   function screen.drawPixel(color)
+      screen.blit("o", color, color)
+   end
 
    function screen.drawLineV(len, color, width)
       if width ~= nil then
@@ -47,26 +47,26 @@ function M.extend_screen(screen)
         end
     end
 
-    function screen.drawLineH(len, color)
-        for _=1,len do
-            screen.drawPixel(color)
-        end
-    end
+   function screen.drawLineH(len, color)
+      for _=1,len do
+         screen.drawPixel(color)
+      end
+   end
 
-    function screen.drawPicture(picture)
-        local lineLength = 0
-        for c in picture:gmatch(".") do
-            if c ~= " " and c ~= "\n" then
-                screen.drawPixel(2^tonumber(c, 16))
-            elseif c == "\n" then
-                screen.moveCursor(-lineLength, 1)
-                lineLength = -1
-            else
-                screen.moveCursor(1, 0)
-            end
-            lineLength = lineLength + 1
-        end
-    end
+   function screen.drawPicture(picture)
+      local lineLength = 0
+      for c in picture:gmatch(".") do
+         if c ~= " " and c ~= "\n" then
+               screen.drawPixel(2^tonumber(c, 16))
+         elseif c == "\n" then
+               screen.moveCursor(-lineLength, 1)
+               lineLength = -1
+         else
+               screen.moveCursor(1, 0)
+         end
+         lineLength = lineLength + 1
+      end
+   end
 
    function screen.drawRect(width, height)
       local basex, basey = screen.getCursorPos()
